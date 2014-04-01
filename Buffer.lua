7d14f5553ba029215caebf6785cf506949389bbb
Pawkette <pawkette.heals@gmail.com>
2014-04-01 06:34:05 -0700

    meow

--- Buffer.lua
+++ Buffer.lua
@@ -5,8 +5,9 @@ function Buffer:Initialize( buffer )
     self.control = buffer
 
     self.control:SetHandler( 'OnLinkClicked', function( ... ) self:HandleLinkClicked( ... ) end )
+    self.control:SetHandler( 'OnMouseenter', function( ... ) Rave.Window:HandleMouseEnter( ... ) end )
 end
 
-function Buffer:HandleLinkClicked(  )
+function Buffer:HandleLinkClicked( ... )
     -- body
 end
